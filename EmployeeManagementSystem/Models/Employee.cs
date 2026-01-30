231e108cae82ea7e76cce8187102bfed3bc15080
ProkopisKamatsos <prokopiskamatsos@gmail.com>
2026-01-30 16:02:07 +0200

    notNull

--- EmployeeManagementSystem/Models/Employee.cs
+++ EmployeeManagementSystem/Models/Employee.cs
@@ -7,10 +7,10 @@ namespace EmployeeManagementSystem.Models
     public class Employee
     {
         public int Id { get; set; }
-        public string FirstName { get; set; }
-        public string LastName { get; set; }
-        public string Email { get; set; }
-        public string Phone { get; set; }
+        public string? FirstName { get; set; }
+        public string? LastName { get; set; }
+        public string? Email { get; set; }
+        public string? Phone { get; set; }
         public int DepartmentId { get; set; }
         public decimal Salary { get; set; }
         public DateTime HireDate { get; set; }
