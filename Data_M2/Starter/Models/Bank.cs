f45f87abe18fa1b78acd3c21819b1a6129dfd38e
ProkopisKamatsos <prokopiskamatsos@gmail.com>
2026-01-31 16:50:08 +0200

    Startin C# Advanced Programming Fundamentals

---
+++ Data_M2/Starter/Models/Bank.cs
@@ -0,0 +1,36 @@
+using System;
+using System.Collections.Generic;
+
+namespace Data_M2;
+
+// TASK 2: Create Bank Class
+// Purpose: Manage customers and transaction reports.
+
+public class Bank
+{
+    // TASK 2: Step 1 - Add bank's unique identifier and customers list
+    // Fields
+    private readonly Guid _bankId;
+    private readonly List<BankCustomer> _customers;
+
+    // Properties
+    public Guid BankId => _bankId;
+    public IReadOnlyList<BankCustomer> Customers => _customers.AsReadOnly();
+
+
+    // TASK 2: Step 2 - Add constructor to initialize properties
+    // Constructors
+    public Bank()
+    {
+        _bankId = Guid.NewGuid();
+        _customers = new List<BankCustomer>();
+    }
+
+    // TASK 2: Step 3 - Implement AddCustomer method
+    //Methods
+    internal void AddCustomer(BankCustomer customer)
+    {
+        _customers.Add(customer);
+    }
+
+}
--- TestCSharp/Program.cs
+++ TestCSharp/Program.cs
@@ -1,23 +1,9 @@
-﻿using System;
-using System.Linq;
-using System.Collections.Generic;
-using Dapper;
-using Microsoft.Data.SqlClient;
-using TestCSharp;
+﻿var product1 = new Product("Laptop", 1200m);
+var product2 = new Product("Laptop", 1200m);
+var product3 = new Product("Tablet", 600m);
+Console.WriteLine($"Are product1 and product2 equal? {product1 == product2}");
+Console.WriteLine($"Are product1 and product3 equal? {product1 == product3}");
+public record Product(string Name, decimal Price);
 
 
-    var ConnectionString =
-        "Server=localhost\\SQLEXPRESS;Database=DapperTrain;Trusted_Connection=True;TrustServerCertificate=True;";
-var sql = "dbo.usp_InsertDapperTest2";
-var sql1 = "SELECT * FROM users";
-using var connection = new SqlConnection(ConnectionString);
-{
-    connection.Open();
-    //var affectedRows = connection.Execute(sql, new { Username = "kostas", Email = "kostas@test.com"  }, commandType: System.Data.CommandType.StoredProcedure);
-    //Console.WriteLine(affectedRows);
-    var  users = connection.Query<User>(sql1).ToList();
-    //foreach (var user in users)
-    //{
-    //    Console.WriteLine($"Id: {user.Id}, Username: {user.Username}, Email: {user.Email}");
-    //}
-}
+
