6abde5b54e1b9aa624f049da354dbb471e3259e5
ProkopisKamatsos <prokopiskamatsos@gmail.com>
2026-02-09 15:25:01 +0200

    bugs Made on ContosoCrafts ASP.NET core app

--- ConstosoCrafts/ConstosoCrafts.WebSite/Services/JsonFileProductService.cs
+++ ConstosoCrafts/ConstosoCrafts.WebSite/Services/JsonFileProductService.cs
@@ -21,38 +21,28 @@ namespace ContosoCrafts.WebSite.Services
         public IEnumerable<Product> GetProducts()
         {
             using var jsonFileReader = File.OpenText(JsonFileName);
-            return JsonSerializer.Deserialize<Product[]>(jsonFileReader.ReadToEnd(),
-                new JsonSerializerOptions
-                {
-                    PropertyNameCaseInsensitive = true
-                });
+            return JsonSerializer.Deserialize<Product[]>(
+                jsonFileReader.ReadToEnd(),
+                new JsonSerializerOptions { PropertyNameCaseInsensitive = true }
+            ) ?? Enumerable.Empty<Product>();
         }
 
         public void AddRating(string productId, int rating)
         {
-            var products = GetProducts();
+            var products = GetProducts().ToArray();
+            var product = products.First(x => x.Id == productId);
 
-            if (products.First(x => x.Id == productId).Ratings == null)
-            {
-                products.First(x => x.Id == productId).Ratings = new int[] { rating };
-            }
-            else
-            {
-                var ratings = products.First(x => x.Id == productId).Ratings.ToList();
-                ratings.Add(rating);
-                products.First(x => x.Id == productId).Ratings = ratings.ToArray();
-            }
+            var ratings = (product.Ratings ?? Array.Empty<int>()).ToList();
+            ratings.Add(rating);
+            product.Ratings = ratings.ToArray();
 
-            using var outputStream = File.OpenWrite(JsonFileName);
+            using var outputStream = File.Create(JsonFileName);
+            using var writer = new Utf8JsonWriter(outputStream, new JsonWriterOptions { Indented = true });
 
-            JsonSerializer.Serialize<IEnumerable<Product>>(
-                new Utf8JsonWriter(outputStream, new JsonWriterOptions
-                {
-                    SkipValidation = true,
-                    Indented = true
-                }),
-                products
-            );
+            JsonSerializer.Serialize(writer, products);
+            writer.Flush();
         }
+
+
     }
 }
