7b09714b612e86c1a2ec748e62f516f863824c84
ProkopisKamatsos <prokopiskamatsos@gmail.com>
2026-03-04 23:58:08 +0200

    Simple CRUD WebApi via Scalar (Controller,SQL Server , EF core,DTOs)

---
+++ VideoGameCharacterApi/Controllers/VideoGameCharactersController.cs
@@ -0,0 +1,51 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using VideoGameCharacterApi.Dtos;
+using VideoGameCharacterApi.Models;
+using VideoGameCharacterApi.Services;
+
+namespace VideoGameCharacterApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class VideoGameCharactersController : ControllerBase
+    {
+        private readonly IVideoGameCharacterService _service;
+
+        public VideoGameCharactersController(IVideoGameCharacterService service)
+        {
+            _service = service;
+        }
+
+
+        [HttpGet]
+        public async Task<ActionResult<List<CharacterResponse>>> GetCharacters()
+            => Ok(await _service.GetAllCharactersAsync());
+
+        [HttpGet("{id}")]
+        public async Task<ActionResult<CharacterResponse>> GetCharacter(int id)
+        {
+            var character = await _service.GetCharacterByIdAsync(id);
+            return character is null ? NotFound("Character with the given Id was not found.") : Ok(character);
+        }
+
+        [HttpPost]
+        public async Task<ActionResult<CharacterResponse>> AddCharacter(CreateCharacterRequest character)
+        {
+            var createdCharacter = await _service.AddCharacterAsync(character);
+            return CreatedAtAction(nameof(GetCharacter), new { id = createdCharacter.Id }, createdCharacter);
+        }
+        [HttpPut("{id}")]
+        public async Task<ActionResult> UpdateCharacter(int id, UpdateCharacterRequest character)
+        {
+            var updated = await _service.UpdateCharacterAsync(id, character);
+            return updated ? NoContent() : NotFound("Character with the given Id was not found.");
+        }
+        [HttpDelete("{id}")]
+        public async Task<ActionResult> DeleteCharacter(int id)
+        {
+            var deleted = await _service.DeleteCharacterAsync(id);
+            return deleted ? NoContent() : NotFound("Character with the given Id was not found.");
+        }
+    }
+}
---
+++ VideoGameCharacterApi/Services/VideoGameCharacterService.cs
@@ -0,0 +1,90 @@
+﻿using Microsoft.EntityFrameworkCore;
+using VideoGameCharacterApi.Data;
+using VideoGameCharacterApi.Dtos;
+using VideoGameCharacterApi.Models;
+
+namespace VideoGameCharacterApi.Services
+{
+    public class VideoGameCharacterService : IVideoGameCharacterService
+    {
+        private readonly AppDbContext _dbContext;
+        public VideoGameCharacterService(AppDbContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+        public async Task<CharacterResponse> AddCharacterAsync(CreateCharacterRequest character)
+        {
+            var newCharacter = new Character
+            {
+                Name = character.Name,
+                Game = character.Game,
+                Role = character.Role
+            };
+
+            _dbContext.Characters.Add(newCharacter);
+            await _dbContext.SaveChangesAsync();
+
+            return new CharacterResponse
+            {
+                Id = newCharacter.Id,
+                Name = newCharacter.Name,
+                Game = newCharacter.Game,
+                Role = newCharacter.Role
+            };
+        }
+
+        public async Task<bool> DeleteCharacterAsync(int id)
+        {
+            var characterToDelete = await _dbContext.Characters.FindAsync(id);
+            if (characterToDelete is null)
+                return false;
+
+            _dbContext.Characters.Remove(characterToDelete);
+            await _dbContext.SaveChangesAsync();
+
+            return true;
+        }
+
+        public async Task<List<CharacterResponse>> GetAllCharactersAsync()
+        {
+            return await _dbContext.Characters
+                .Select(c => new CharacterResponse
+                {
+                    Id = c.Id,
+                    Name = c.Name,
+                    Game = c.Game,
+                    Role = c.Role
+                })
+                .ToListAsync();
+        }
+
+        public async Task<CharacterResponse?> GetCharacterByIdAsync(int id)
+        {
+            var character = await _dbContext.Characters.Where(c => c.Id == id)
+                                                        .Select(c => new CharacterResponse
+                                                        {
+
+                                                            Name = c.Name,
+                                                            Game = c.Game,
+                                                            Role = c.Role
+                                                        }).FirstOrDefaultAsync();
+
+            return character;
+        }
+
+        public async Task<bool> UpdateCharacterAsync(int id, UpdateCharacterRequest character)
+        {
+            var existingCharacter = await _dbContext.Characters.FindAsync(id);
+            if (existingCharacter is null)
+                return false;
+
+            existingCharacter.Name = character.Name;
+            existingCharacter.Game = character.Game;
+            existingCharacter.Role = character.Role;
+
+            await _dbContext.SaveChangesAsync();
+
+            return true;
+        }
+    }
+}
